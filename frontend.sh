5b0cf048e897b323d14a2a21e69932f97f781416
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-09 13:43:38 +0530

    frontend file

---
+++ frontend.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USER_VALID
+
+echo "now you are in the Frontend module"
+
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "installing nginx"
+
+systemctl enable nginx &>>$LOG_FILE
+VALIDATE $? "enabling nginx"
+
+systemctl start nginx &>>$LOG_FILE
+VALIDATE $? "stsrting nginx"
+
+rm -rf /user/share/nginx/html/* &>>$LOG_FILE
+VALIDATE $? "removing the default content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$loading
+VALIDATE $? "downlode the frontend code"
+
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "unzip the frontend code"
+
+cp /home/ec2-user/exp-11-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOG_FILE
+VALIDATE $? "coping the expense.conf file"
+
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "restarting the nginx"
