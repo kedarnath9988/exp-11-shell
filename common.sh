a4a15b9a07995ad7592f057fc3b0eaaaceadd78a
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-09 10:08:29 +0530

    Mysql.sh

---
+++ common.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+
+USER=$(id -u )
+TIME=$( data +%F-%H-%M-%S )
+SCRIPT=$( echo $0 | cut -d "." -f1 )
+LOG_FILE=/tmp/$SCRIPT-$TIME.log
+
+# colours 
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$G $2 done successfully .. $N"
+    else 
+        echo -e "$R $@ failure .. $N"
+        exit 1 
+}
+
+echo "script is " $0 
+
+USER_VALID(){
+    if [ $USER -eq 0 ]
+    then
+        echo -e "$G you are super-user $N"
+    else 
+        echo -e "$R need super user access $N"
+        exit 1 
+    fi 
+}
