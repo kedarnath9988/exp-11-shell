1d53c96a09d41e6f06facc998ca31feb47d2d419
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-09 13:11:28 +0530

    Mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -5,4 +5,24 @@ source ./common.sh
 
 USER_VALID
 
-echo "from "$0
+
+echo "please enter the mysql password"
+read PASSWORD_MYSQL   
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "installing mysqql-server"
+
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? " enabling mysql "
+
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "start mysqld "
+
+mysql -h db.dawskedarnath.online -uroot -p${PASSWORD_MYSQL} -e 'show databases;' &>>$LOG_FILE
+if [ $? -eq 0 ]
+then 
+    echo  -e "$G  password already setuped successfully $N"
+else 
+    mysql -h db.dawskedarnath.online --set-root-pass ${PASSWORD_MYSQL}  &>>$LOG_FILE
+    VALIDATE $? "installing mysqql-server"
+fi 
